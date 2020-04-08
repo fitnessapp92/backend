d6fa06a209286a33cc7b1a29eb34ae28387ab201
Vladimir Cravcenco <VladimirKravchenco@gmail.com>
2020-04-08 15:32:33 +0300

    init commit

---
+++ tests/Manual/MyTest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Tests;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class MyTest extends KernelTestCase
+{
+    public function testMyTest(){
+        return;
+    }
+}
